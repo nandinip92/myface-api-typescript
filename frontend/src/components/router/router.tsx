fc4f69c38fcb41ea794c742649e8c6662a500475
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-07 15:18:42 +0000

    feat: creating a route based on the userId

--- frontend/src/components/router/router.tsx
+++ frontend/src/components/router/router.tsx
@@ -1,12 +1,14 @@
 import { Route,Routes } from "react-router-dom";
 import { PostsPage } from "../posts_page/posts_page";
 import { UsersPage } from "../users_page/users_page";
+import { UserDetails } from "../user_details/user_details";
 
 export const Router=()=>{
     return(
     <Routes>
         <Route path="/posts" element={<PostsPage/>}/>
         <Route path="/users" element={<UsersPage/>}/>
+        <Route path="/users/:id" element={<UserDetails/>}/>
     </Routes>
     );
 }
