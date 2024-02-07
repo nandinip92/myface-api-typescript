326691381ecc721c88ef9b6ef485635ec1df9a8c
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-07 12:22:52 +0000

    feat: Adding userModels

---
+++ frontend/src/models/usersModel.ts
@@ -0,0 +1,24 @@
+import moment from "moment";
+
+interface UserPostModel {
+    id: number;
+    message: string;
+    imageUrl: string;
+    createdAt: moment.Moment;
+}
+
+export interface UserModel {
+    id: number;
+    name: string;
+    username: string;
+    profileImageUrl: string;
+    coverImageUrl: string;
+    email: string;
+    posts: UserPostModel[];
+    likes: UserPostModel[];
+    dislikes: UserPostModel[];
+}
+
+export interface ApiUsers{
+    results:Array<UserModel>
+}
