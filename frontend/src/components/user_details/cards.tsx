563ab7ba2bc225400cc02721bee92403010859e5
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-07 18:15:23 +0000

    feat: Adding classes to the tags for styling

--- frontend/src/components/user_details/cards.tsx
+++ frontend/src/components/user_details/cards.tsx
@@ -1,26 +1,25 @@
 import React from "react";
 import { UserPostModel } from "../../models/usersModel";
 
-interface CardsProps{
-    username:string;
-    postCards:Array<UserPostModel>
+interface CardsProps {
+  username: string;
+  postCards: Array<UserPostModel>;
 }
-export const Cards: React.FC<CardsProps>=({username,postCards})=>{
-
-return (
+export const Cards: React.FC<CardsProps> = ({ username, postCards }) => {
+  return (
     <ul className="cards-container">
-        {postCards.map(card=>(
-            <li className="cards-item">
-                <div className="card">
-                    <img src= {card.imageUrl}/>
-                    <div className="card-content">
-                        {username}
-                        {card.createdAt}
-                        {card.message}
-                    </div>
-                </div>
-            </li>
-        ))}
+      {postCards.map((card) => (
+        <li className="cards-item">
+          <div className="card">
+            <img src={card.imageUrl} />
+            <div className="card-content">
+              <p className="sub-title">{username}</p>
+              <p className="secondary">{card.createdAt}</p>
+              <p className="card-message">{card.message}</p>
+            </div>
+          </div>
+        </li>
+      ))}
     </ul>
-)
-}
+  );
+};
--- frontend/src/components/user_details/user_profile_body.tsx
+++ frontend/src/components/user_details/user_profile_body.tsx
@@ -20,15 +20,15 @@ export const UserProfileBody: React.FC<UserProfileBodyProps> = ({
   return (
     <div>
       <div className="posts">
-        <h1>{firstName}'s Posts</h1>
+        <h1 className="title">{firstName}'s Posts</h1>
         <Cards username={username} postCards={posts} />
       </div>
       <div className="likes">
-        <h1>Posts {firstName} liked</h1>
+        <h1 className="title">Posts {firstName} liked</h1>
         <Cards username={username} postCards={likes} />
       </div>
       <div className="dislikes">
-        <h1>Posts {firstName} disliked</h1>
+        <h1 className="title">Posts {firstName} disliked</h1>
         <Cards username={username} postCards={dislikes} />
       </div>
     </div>
