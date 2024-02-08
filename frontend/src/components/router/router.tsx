d69346cbabb51d15bf75f4432025534379288f32
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-08 19:39:54 +0000

    feat: Adding new route to the create new user form

--- frontend/src/components/router/router.tsx
+++ frontend/src/components/router/router.tsx
@@ -2,6 +2,7 @@ import { Route,Routes } from "react-router-dom";
 import { PostsPage } from "../posts_page/posts_page";
 import { UsersPage } from "../users_page/users_page";
 import { UserDetails } from "../user_details/user_details";
+import { CreateNewUser } from "../forms/create_new_user";
 
 export const Router=()=>{
     return(
@@ -9,6 +10,7 @@ export const Router=()=>{
         <Route path="/posts" element={<PostsPage/>}/>
         <Route path="/users" element={<UsersPage/>}/>
         <Route path="/users/:id" element={<UserDetails/>}/>
+        <Route path="/users/create" element={<CreateNewUser/>}/>
     </Routes>
     );
 }
