f1b38f601cf0c6f52202e0225fe9ff445fca3274
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-09 10:28:24 +0000

    feat: Adding NewUserForm compement route

--- frontend/src/components/router/router.tsx
+++ frontend/src/components/router/router.tsx
@@ -3,7 +3,7 @@ import { PostsPage } from "../posts_page/posts_page";
 import { UsersPage } from "../users_page/users_page";
 import { UserDetails } from "../user_details/user_details";
 import { NewUserForm } from "../forms/new_user_form";
-import { CreateNewUser } from "../forms/create_new_user";
+//import { CreateNewUser } from "../forms/create_new_user";
 
 
 export const Router=()=>{
@@ -12,7 +12,6 @@ export const Router=()=>{
         <Route path="/posts" element={<PostsPage/>}/>
         <Route path="/users" element={<UsersPage/>}/>
         <Route path="/users/:id" element={<UserDetails/>}/>
-        <Route path="/users/create_old" element={<CreateNewUser/>}/>
         <Route path="/users/create" element={<NewUserForm/>}/>
     </Routes>
     );
